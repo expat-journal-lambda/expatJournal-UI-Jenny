42e1beed6eeac9b0e2e9a5d7c55f83949429c3e2
Jenny Li <jli226@fordham.edu>
2019-06-27 13:37:01 -0700

    fix js

--- js/index.js
+++ js/index.js
@@ -8,32 +8,12 @@ logoHead.addEventListener("mouseout", function() {
   logoHead.style.color = "black";
 });
 
-const img = document.querySelector(".intro img");
-
-img.addEventListener("click", function() {
-  img.src = "img/funbusgif.gif";
-});
-
-img.addEventListener("dblclick", function() {
-  img.src = "img/fun-bus.jpg";
-});
-
-window.addEventListener("contextmenu", function(x) {
-  alert("WHATCHU RIGHT CLICKIN");
-});
-
 const body = document.querySelector("body");
 const nav = document.querySelector(".main-navigation");
 body.addEventListener("keydown", function() {
   nav.style.position = "absolute";
 });
 
-body.addEventListener("keypress", function() {
-  nav.style.position = "fixed";
-});
-
 body.addEventListener("copy", function(x) {
-  alert(`You copied somethin
+  alert(`You copied something`);
 });
-
-
